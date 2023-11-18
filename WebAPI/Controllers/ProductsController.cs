4293a1d1978f458b21e956e0420219d65fd0af39
OzanAhmetDincer <ozanahmetdincer@gmail.com>
2023-11-18 20:40:41 +0300

    API başlangıç

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    // Route yönlentirme yapar yapi url kısmında domainden sonra "api/Products" yazarsa gelip bu controller'i çalıştıracak
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Loosely coupled
+        // naming convention
+        // IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            // Biz burada IProductService parametresi verdik ve ProductManager class'ından nesne türetip işlemlerimizi ProductManager üzerinden yapmamız lazım. Fakat IProductService bunu bilemediği için yapamaz ve hata verir. O yüzden Program.cs de AddSingleton metodunu kullanarak bu istediğimizi yapabiliriz.
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        //[HttpGet("{id}")] bu şekilde id vererek de yazdığımız metotda yönlendirme yapabiliriz. Yada her metodumuza ayrı isim verip ve httpget gibi isteklerine de isim vererek bu isimler üzerinden çağırırız.
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,37 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --> IoC Container iþlemleri için kullanýlabilecek yapýlar. AOP denir.
+// IoC Container yapýsýný kullanýrýz. Bir kutu düþün ve bu kutu içerisinde ProductManage, EfProductDal vb. yapýlar var ve biz bunlarýn implamente edildiði interface'ler üzerinden bu class'larý çaðýrdýðýmýzda otomatik, bizim yerimize new iþlemini yapar. Aþaðýda da þunu demiþ oluruz birisi bizden constructorda IProductService isterse ona ProductManager oluþtur ve referansýný ver demiþ oluruz. AddSingleton ile daha performnslý bir iþlem yapmýþ oluruz. Þuan yaptýðýmýz örnekte AddSingleton tüm bellekte bir tane ProductManager oluþturuyor ve kaç tane istek gelirse gelsin hepsine ayný instance'yi veriyor ve iþlemleri bunun üzerinden yapýyor, hepsi ayný referansý kullanýyor. Fakat AddSingleton metodunu Manager class'ýnda bir veri tutmuyorsak yaparýz. Yoksa herkese ayný veri gider ve biri bir þey yapýnca diðerlerinde de ayný iþlem gerçekleþir. Verileri veri tabanýnda tutunca her hangi bir þey olmaz. ProductManager da içerisinde IProductDal kullandýðý için burada da bir new'leme iþlemi var. O yüzden data katmaný içinde yapýlýr.(IProductDal, EfProductDal)
+builder.Services.AddSingleton<IProductService, ProductManager>();
+builder.Services.AddSingleton<IProductDal, EfProductDal>();
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
